d3daeb0405ce07765433a123bb4c98fc6f8cad95
xiaolu hong <xiaoluhong@rancher.com>
2020-07-31 14:29:33 +0800

    去除旧版本删除逻辑

--- start.sh
+++ start.sh
@@ -17,7 +17,8 @@ k3s()
     repo=rancher/k3s
 
     echo '获取前三个 releases 版本'
-    k3s_new_ver=$( curl -LSs https://api.github.com/repos/$repo/git/refs/tags | jq -r .[].ref | awk -F/ '{print $3}'  | grep -v -E 'rc|alpha' | tail -n 5 )
+    k3s_new_ver=$( curl -LSs https://api.github.com/repos/$repo/git/refs/tags | jq -r .[].ref | awk -F/ '{print $3}'  | grep -v -E 'rc|alpha' | tail -n 3 )
+    echo "${k3s_new_ver}"
 
     echo '通过 releases 获取文件下载链接'
     for new_ver in ${k3s_new_ver};
@@ -38,25 +39,26 @@ k3s()
             wget -q ${url}
             cd ${DIR}
         done
+
         ls download/`echo ${new_ver} | sed 's/+/-/g'`
 
         echo "上传 ${new_ver}"
         ${DIR}/ossutil ${auth_command} cp -r download/`echo ${new_ver} | sed 's/+/-/g'` oss://${bucket_name}/download/`echo ${new_ver} | sed 's/+/-/g'` -u
     done
 
-    # 列出 OSS 中已有版本
-    k3s_old_ver=$( ${DIR}/ossutil ${auth_command} ls oss://${bucket_name}/download/ -d -s | grep "oss://${bucket_name}/download/v" | awk -F'/' '{print $5}' )
+    ## 列出 OSS 中已有版本
+    #k3s_old_ver=$( ${DIR}/ossutil ${auth_command} ls oss://${bucket_name}/download/ -d -s | grep "oss://${bucket_name}/download/v" | awk -F'/' '{print $5}' )
 
-    echo '用 OSS 中已有的版本号与获取的新版本号做匹配，不匹配的则删除'
-    for old_ver in ${k3s_old_ver};
-    do
-        if ! echo ${k3s_new_ver} | sed 's/+/-/g' | grep -w ${old_ver} > /dev/null; then
-            echo "删除老版本: ${old_ver}"
-            ${DIR}/ossutil ${auth_command} rm oss://${bucket_name}/download/${old_ver}/ -r -f
-        else
-            echo '没有新版本增加，无需删除'
-        fi
-    done
+    #echo '用 OSS 中已有的版本号与获取的新版本号做匹配，不匹配的则删除'
+    #for old_ver in ${k3s_old_ver};
+    #do
+    #    if ! echo ${k3s_new_ver} | sed 's/+/-/g' | grep -w ${old_ver} > /dev/null; then
+    #        echo "删除老版本: ${old_ver}"
+    #        ${DIR}/ossutil ${auth_command} rm oss://${bucket_name}/download/${old_ver}/ -r -f
+    #    else
+    #        echo '没有新版本增加，无需删除'
+    #    fi
+    #done
 }
 
 k3s
